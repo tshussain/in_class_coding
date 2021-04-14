335e39d8dd041e0911b6d6d1b291f2169f7c34c0
Talib Hussain <talibshussain@hotmail.com>
2021-04-14 09:31:55 -0400

    Test change for pushing/pulling test.

--- src/main/java/com/jac/in_class_coding/InClassCodingApplication.java
+++ src/main/java/com/jac/in_class_coding/InClassCodingApplication.java
@@ -17,6 +17,7 @@ public class InClassCodingApplication {
 
         // Day 1 example
         InClassExamples.switchExample();
+        InClassExamples.switchExample();
 
         // Examples of accessing the same object as itself or as the interface it implements
         // uncomment out these 3 lines to see what works and what doesn't
