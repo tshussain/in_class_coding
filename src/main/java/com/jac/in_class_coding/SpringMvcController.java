b4936f2b66204f8d365d8843a67394d1e9a979aa
Talib Hussain <talibshussain@hotmail.com>
2021-04-26 13:37:10 -0400

    Add list all customers from database

--- src/main/java/com/jac/in_class_coding/SpringMvcController.java
+++ src/main/java/com/jac/in_class_coding/SpringMvcController.java
@@ -2,6 +2,8 @@ package com.jac.in_class_coding;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,17 @@ public class SpringMvcController {
         customer.setAddress("");
         customerRepository.save(customer);
         return "Added customer to repo";
+    }
+
+    @GetMapping("/list")
+    public String getCustomers(ModelMap model) {
+        // Retrieve the information from the database and associate it with the
+        //   attribute "customers" in the model.  This attribute needs to correspond
+        //   to the name used in the html file that will be loaded.
+        model.addAttribute("customers", customerRepository.findAll());
+
+        // This is the name of the html file to display (passing the info in the model).
+        return "list_customers";
     }
 
 }
