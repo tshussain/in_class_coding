281f10c77eb8a97195ed46bb82033c6d18f14675
Talib Hussain <talibshussain@hotmail.com>
2021-04-27 10:31:45 -0400

    Add "update" to change customer name. Include some documentation explaining @ResponseBody and GET vs POST

--- src/main/java/com/jac/in_class_coding/SpringMvcController.java
+++ src/main/java/com/jac/in_class_coding/SpringMvcController.java
@@ -8,13 +8,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @Controller
 public class SpringMvcController {
 
     @Autowired
     private CustomerRepository customerRepository;
 
-    @PostMapping("/add")
+    /* Returns a message to display directly on the page.
+    *    Since this is returning a specific "response", we use @ResponseBody */
+    // Post show this in the URL: http://localhost:8082/add
+    // Get shows this in the URL: http://localhost:8082/add?name=Homer&age=13
+    @GetMapping("/add")
     @ResponseBody
     public String addCustomer(String name, int age) {
         Customer customer = new Customer();
@@ -25,6 +31,8 @@ public class SpringMvcController {
         return "Added customer to repo";
     }
 
+    /** Returns a 'view' (i.e., the name of an html file).
+     *    Since it is a view, we do NOT use @ResponseBody */
     @GetMapping("/list")
     public String getCustomers(ModelMap model) {
         // Retrieve the information from the database and associate it with the
@@ -34,6 +42,22 @@ public class SpringMvcController {
 
         // This is the name of the html file to display (passing the info in the model).
         return "list_customers";
+    }
+
+    /** Returns a success/fail message to be displayed on the page.  We need to use @ResponseBody */
+    @GetMapping("/updateName")
+    @ResponseBody
+    public String updateCustomerName(int id, String newName) {
+        Optional<Customer> customerToChange = customerRepository.findById(id);
+
+        if (customerToChange.isPresent()) {
+            Customer customerObject = customerToChange.get();
+            customerObject.setName(newName);
+            customerRepository.save(customerObject);
+            return "Customer name successfully changed";
+        } else {
+            return "Customer id " + id + " is not valid - not in database";
+        }
     }
 
 }
